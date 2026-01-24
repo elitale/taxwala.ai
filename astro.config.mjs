edb68cb1a0527dbaf2f08366ab35910280ba8dd9
Dharmendra Soni <dharmendra.soni@linqhaul.com>
2026-01-24 21:51:02 +0530

    feat: typescript code

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,18 @@ import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
+
+import sitemap from '@astrojs/sitemap';
+
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+
+  site: "https://taxwala.ai",
+  integrations: [react(), sentry(), spotlightjs(), sitemap(), robotsTxt()],
 
   vite: {
     plugins: [tailwindcss()]
